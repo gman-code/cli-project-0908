9f80c1c0a2f452e57e60144c8d910b6a3580f763
gman-code <67984470+gman-code@users.noreply.github.com>
2020-10-02 19:41:25 -0400

    Update cli.rb

--- cli.rb
+++ cli.rb
@@ -1,23 +1,37 @@
 class CLI
    
-    def cat_facts
-       
+    def breed
         puts ""
         puts "welcome to cats facts"
         puts "***************"
         puts ""
-        puts "Write a fact about your cat or 'exit' to exit."
+        puts "Enter a fact about your cat or 'exit' to exit."
         puts ""
-        API.get_cats
-        @facts= gets.strip.downcase
-        cat = Fact.all
-        cat.each.with_index(1) do |c, i|
-           puts "{i}. #{c.name}"
-        end
+        API.get_breed
+        @breed= gets.strip.downcase
+        print_breed
+        prompt
+        input = gets.strip.downcase 
+        while input != 'exit' do
+            if input == 'origin'   
 
+            elsif input.to_i > 0 && input.t_i <= Cat.find_by_origin(@origin).length    
+                cat = Cat.find_by_origin(@origin)[inputs.to_i-1]    
+                API.get_breed 
 
-        def end_program 
-            puts "Hey, see you later!"
+            end
         end
-     end 
-end
+    end 
+
+    def prompt  
+        puts ""
+        puts "Enter a number about your cat or 'exit' to exit."
+        puts ""
+    end 
+
+    def print_breed 
+        Cat.find_by_origin(@origin).each.with_index(1) do |c, i|
+            puts "#{i}. #{c.name}"
+        end
+    end 
+end 
