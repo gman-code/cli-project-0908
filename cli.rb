e643d3e9ecc06e183f0afa29de7bc4745e84ac6d
gman-code <67984470+gman-code@users.noreply.github.com>
2020-10-01 21:08:54 -0400

    Update cli.rb

--- cli.rb
+++ cli.rb
@@ -10,13 +10,14 @@ class CLI
         puts ""
         API.get_cats
         @facts= gets.strip.downcase
-        
+        cat = Fact.all
+        cat.each.with_index(1) do |c, i|
+           puts "{i}. #{c.name}"
+        end
 
 
         def end_program 
             puts "Hey, see you later!"
         end
-
-
-    end 
+     end 
 end
