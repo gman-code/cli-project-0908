e75ecf6827e541e435592148ed03178a86a355cd
gman-code <67984470+gman-code@users.noreply.github.com>
2020-10-02 19:44:13 -0400

    Update api.rb

--- api.rb
+++ api.rb
@@ -1,12 +1,15 @@
 class API
 
-    def self.get_cats
+    def self.get_cat
         url="https://api.thecatapi.com/v1/breeds"
         uri=URI(url)
         response=Net::HTTP.get(uri)
-        cats=JSON.parse(response)["cats"].each do |a|
-           Fact.new(name: a["Abyssinian"], cats_id: c["abys"], origin: c["Egypt"], fact: fact) if a["Abyssinian"] != nil
+
+        cat=JSON.parse(response)
+        cat.each do |c|
+           Cat.new(name: c["name"], temperament:  c["temperament"], origin: c["origin"], description: c["description"] ) if c["name"] != ""
         # use cats info to make a Fact for each type of cat
         end
-    end
+    end 
+    
 end
